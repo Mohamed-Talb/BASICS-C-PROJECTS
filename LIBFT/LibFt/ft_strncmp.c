5ae1e9176c3173218483a2707d900b56f0e94411
Mohamed-Talb <mtaleb.contact@gmail.com>
2026-05-21 20:16:39 +0100

    Fix strncompare  in libft

--- LIBFT/LibFt/ft_strncmp.c
+++ LIBFT/LibFt/ft_strncmp.c
@@ -18,8 +18,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	i = 0;
 	while ((i < n) && s1[i] && s2[i] && s1[i] == s2[i])
+	{
 		i++;
+	}
 	if (i < n)
+	{
 		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
 	return (0);
 }
